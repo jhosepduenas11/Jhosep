1268623302a2db95a48b65c948356a5de5d55003
jhosepduenas11 <jhosepduenas11@gmail.com>
2026-08-13 16:50:18 -0500

    Cambioxd

--- RepasoG3/src/main/java/pe/edu/upeu/repasog3/ArreglosVM.java
+++ RepasoG3/src/main/java/pe/edu/upeu/repasog3/ArreglosVM.java
@@ -22,7 +22,7 @@ public class ArreglosVM {
 
     public static void imprimirMatriz(int [][] m){
         for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
+            for (int j = 0; j < m[1].length; j++) {
                 if(m[i][j]!=0){
                     System.out.print(m[i][j]+"\t");
                 }else{
@@ -34,10 +34,54 @@ public class ArreglosVM {
 
     }
 
+    public static int [][] matrizF5(int tamano, int numI ){
+        int [][]matriz = new int [tamano][tamano];
+        for ( int i=0; i < matriz . length; i++){
+            for (int j =matriz [0].length-1; j >=matriz [0].length-1 - i; j--) {
+                matriz[i][j]=numI;
+                numI++;
+
+            }
+        }
+        return matriz;
+    }
+
+    public static int [][] matrizF11(int tamano, int numI ){
+        int [][]matriz = new int [tamano][tamano];
+        for (int f = 0; f < matriz.length; f++) {
+            for(int c= matriz[0].length-1-f; c>=0; c--){
+                matriz[f][c]=numI;
+                numI++;
+
+            }
+
+        }
+        return matriz;
+    }
+
+    public static int [][] matrizF12(int tamano, int numI ){
+        int [][]matriz = new int [tamano][tamano];
+        for (int d = 0; d < matriz.length; d++) {
+            for(int p= matriz[0].length-1-d; p>=0; p--){
+                matriz[d][p]=numI;
+                numI++;
+
+            }
+
+        }
+        return matriz;
+    }
+
 
     public static void main(String[] args) {
-        meses();
-        int [][] m={{0,2}, {3,4}};
-        imprimirMatriz(m);
+        //meses();
+        //int [][] m={{1,2,6}, {3,4,2}, {5,6,7}};
+        //imprimirMatriz(m);
+        imprimirMatriz( matrizF5 (6, 1));
+        System.out.println("");
+        imprimirMatriz(matrizF11 (5 , 1 ));
+        System.out.println("");
+        imprimirMatriz(matrizF12 (5, 1));
+
     }
 }
